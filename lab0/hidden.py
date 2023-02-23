4dbe6fd5eb0bee6a41ff4dccbc416edb975baed3
James <jchen.6727@gmail.com>
2023-02-22 21:42:29 -0500

    try out hidden with curl / wget? github raw

---
+++ lab0/hidden.py
@@ -0,0 +1,16 @@
+class hidden:
+# hidden message 1: taked baby meet at bar night or day sometime
+
+    message = ['-'] * 50
+
+    def test0(self, guess, answer):
+        if guess == answer:
+            print("correct!")
+            self.message[]
+        else:
+            print("sorry! try again")
+        
+
+    def get_phrase(self):
+        print("").join("{}".format(i for i in messagearr))
+    
